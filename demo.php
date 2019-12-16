b8af15e53072bc1eadee36e107fb60be3e1a1027
AsafRollout <35894775+AsafRollout@users.noreply.github.com>
2019-12-16 12:48:41 +0200

    removing the exception for  getStateCdnPath

--- demo.php
+++ demo.php
@@ -54,7 +54,6 @@ if (!isset($_GET['nocache'])) {
         ->setHttpClientFactory(new GuzzleHttpClientFactory(
                 (new GuzzleHttpClientOptions())
                     ->setLogCacheHitsAndMisses(true)
-                    ->setNoCachePaths([Environment::getStateCdnPath()]) // Don't cache state report requests
                     ->addMiddleware(new CacheMiddleware(
                         new GreedyCacheStrategy(
                             new DoctrineCacheStorage(
